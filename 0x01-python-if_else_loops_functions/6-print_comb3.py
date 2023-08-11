81c283fc42f7cd0869b8edfb7651de96a9026472
Sbucysiwe <sbucysiwe@gmail.com>
2023-08-10 18:22:01 -0700

    alx-higher_level_programming

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 # 6-print_comb3.py
 
-"""Generate all distinct pairs of ascending digits.
-
-Ensure that the pairs consist of different digits; pairs like 01 and 10 are to be treated as the same.
-"""
-
-pairs = []
-
-for digit1 in range(0, 9):  # Update the range to go up to 9 for digit1
+"""Generate all distinct pairs of ascending digits."""
+for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
-        pairs.append("{}{}".format(digit1, digit2))
-
-print(", ".join(pairs))  # Join the list elements with commas and print
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
