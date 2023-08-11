2a1844b38af4908ad7e913bd84aa304dc7d84ff5
Sbucysiwe <sbucysiwe@gmail.com>
2023-08-10 18:28:14 -0700

    alx-higher_level_programming

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@
 
 
 def print_last_digit(number):
-"""Display the last digit of a number than return it"""
-print(abs(number) % 10, end="")
+    """Display the last digit of a number than return it."""
+    print(abs(number) % 10, end="")
     return (abs(number) % 10)
