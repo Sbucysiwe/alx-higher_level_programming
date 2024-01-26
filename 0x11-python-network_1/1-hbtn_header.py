001e7e914e41bcb235d3c4db61aa54b8ff10e8f5
PlexSir <88951611+PlexSir@users.noreply.github.com>
2024-01-26 06:08:59 +0200

    alx-higher_level_programming

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Shows X-Request-Id header variable of request to given URL.
+Usage: ./1-hbtn_header.py <URL>
+"""
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,9 @@
+Project Teaching:
+
+Fetching internet resources with the Python package urllib
+Decoding urllib body response
+using  Python package requests #requestsiswaysimplerthanurllib
+Making HTTP GET request
+Making HTTP POST/PUT/etc. request
+Fetching JSON resources
+Manipulating data from an external service
